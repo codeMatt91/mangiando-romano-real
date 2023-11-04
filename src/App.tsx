05afb8ab19158e38e3013f5844c7d6e25cf1d7cd
codeMatt91 <matteo.imbimbo@yahoo.it>
2023-11-04 16:22:50 +0100

    Create App React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+import Restaurants from "./Components/Restaurants";
+import Login from "./Components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/restaurants"
+            element={<Restaurants />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="select-page-container">
+      Ciccio
+    </div>
+  );
+}
+
+export default Login;
